e20d977b106d410b9e3bc5aeb7483068edc1664d
hillmiao <miaowei2008@163.com>
2016-01-12 11:47:48 +0800

    Added a web configuration file

---
+++ config.py
@@ -0,0 +1,10 @@
+
+urlmapping = [
+        (r'/', 'controllers.main.index'),
+        (r'/main', 'controllers.main.main'),
+        ]
+
+db_ip = 'localhost'
+db_user = 'root'
+db_password = 'root'
+db_name = 'test'
